93da373adb229e711506dd44a5c971a9f4e603cd
Pamyk <pamyk.ch@gmail.com>
2019-06-05 17:24:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const newHeader = document.createElement('h1')
+
+document.body.appendChild(newHeader)
+
+newHeader.id = 'victory'
+
+newHeader.innerHTML = "Pamyk is the champion"
